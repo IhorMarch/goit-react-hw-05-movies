f0c7ac8db07edaaacd3c61a0b2622beb24042b25
IhorMarch <97352675+IhorMarch@users.noreply.github.com>
2023-09-19 00:42:04 +0300

    fin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import { Route, Routes, } from 'react-router-dom';
+import { Layout } from './Layout';
+import { GlobalStyle } from './GlobalStyles';
+import { lazy } from 'react';
+
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const Cast = lazy(() => import('../pages/Cast'));
+const Reviews = lazy(() => import('../pages/Reviews'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  <>
+    <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index  element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+               <Route path="cast" element={<Cast />} />
+               <Route path="reviews" element={<Reviews />} />
+          </Route>
+       
+        
+
+       <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
+    <GlobalStyle />
+    </>
+    
+ 
   );
-};
+}
